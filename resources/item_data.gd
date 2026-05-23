533a1ce2b52cc89608561b250d9793a57636c069
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-05-23 13:31:08 +0200

    Set up basic shapes

--- resources/item_data.gd
+++ resources/item_data.gd
@@ -4,5 +4,53 @@ extends Resource
 @export var name: String
 @export var sprite: Texture2D
 @export var illegal: bool
-@export var shape: Array[Array]
+@export var shape: Shape = Shape.SINGLE
 @export var origin: Vector2
+
+enum Shape {
+	SINGLE,	## 1x1
+	TALL,	## 1x2
+	LONG,	## 2x1
+	SQUARE,	## 2x2
+	VTALL,	## 1x3
+	VLONG,	## 3x1
+	WIDE,	## 3x2
+	LARGE,	## 3x3
+}
+
+const SHAPE_DATA: Dictionary[Shape, Array] = {
+	Shape.SINGLE: [	[0, 0, 0],
+					[0, 0, 0],
+					[1, 0, 0]],
+
+	Shape.TALL: [	[0, 0, 0],
+					[1, 0, 0],
+					[1, 0, 0]],
+
+	Shape.LONG: [	[0, 0, 0],
+					[0, 0, 0],
+					[1, 1, 0]],
+
+	Shape.SQUARE: [	[0, 0, 0],
+					[1, 1, 0],
+					[1, 1, 0]],
+
+	Shape.VTALL: [	[1, 0, 0],
+					[1, 0, 0],
+					[1, 0, 0]],
+
+	Shape.VLONG: [	[0, 0, 0],
+					[0, 0, 0],
+					[1, 1, 1]],
+
+	Shape.WIDE: [	[0, 0, 0],
+					[1, 1, 1],
+					[1, 1, 1]],
+
+	Shape.LARGE: [	[1, 1, 1],
+					[1, 1, 1],
+					[1, 1, 1]],
+}
+
+func get_shape_data() -> Array:
+	return SHAPE_DATA[shape]
