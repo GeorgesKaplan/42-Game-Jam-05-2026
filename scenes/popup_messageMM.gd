b9655b4f25c02dbc8e31dce3888a1aef10a8f82b
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-05-24 19:19:17 +0200

    Backup

---
+++ scenes/popup_messageMM.gd
@@ -0,0 +1,19 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_return_game_button_button_up() -> void:
+	visible = false
+
+
+func _on_quit_game_button_button_up() -> void:
+	visible = true
