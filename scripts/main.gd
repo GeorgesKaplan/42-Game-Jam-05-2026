4c151af73d1e94ab20193a1158811f3a7f38cd98
Shuriken_n <romane.git@redstudio.dev>
2026-05-24 18:28:23 +0200

    More UI
    Added Quit button, on main menu and pause menu, with a confirmation message.
    Added the font

--- scripts/main.gd
+++ scripts/main.gd
@@ -25,11 +25,12 @@ func _process(delta: float) -> void:
 	value += delta
 	if (value >= 1.0):
 		value -= 1.0
-		print("Roll for Sound")
 		if (randf() <= 0.05):
 			$BoatCreaking.play()
-			print("Sound played")
+
+func _on_quit_game_button_button_up() -> void:
+	get_tree().quit()
 
 
-func _on_start_game_button_button_down() -> void:
-	pass
+func _on_quit_game_button_2_button_up() -> void:
+	get_tree().quit()
