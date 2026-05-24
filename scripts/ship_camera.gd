808e57feca58bd3ab41297d4cc70b4e7f3c8ce03
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-05-24 15:34:26 +0200

    Functional click to drag

--- scripts/ship_camera.gd
+++ scripts/ship_camera.gd
@@ -1,16 +1,31 @@
 extends Camera2D
 
+var _is_dragging: bool = false
+var _last_mouse_position: Vector2 = Vector2.ZERO
+
 func _input(event: InputEvent) -> void:
 	@warning_ignore("unsafe_property_access")
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton:
 		@warning_ignore("unsafe_property_access")
-		if event.button_index == MOUSE_BUTTON_WHEEL_UP :
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			@warning_ignore("unsafe_property_access")
+			_is_dragging = event.pressed
+			@warning_ignore("unsafe_property_access")
+			_last_mouse_position = event.position
+			return
+		@warning_ignore("unsafe_property_access")
+		if event.pressed and event.button_index == MOUSE_BUTTON_WHEEL_UP :
 			print("Scroll wheel up")
 			zoom_in(true)
 		@warning_ignore("unsafe_property_access")
-		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+		if event.pressed and event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
 			print("Scroll wheel down")
 			zoom_in(false)
+	elif event is InputEventMouseMotion and _is_dragging:
+		@warning_ignore("unsafe_property_access")
+		global_position -= event.position - _last_mouse_position
+		@warning_ignore("unsafe_property_access")
+		_last_mouse_position = event.position
 
 func zoom_in(is_in: bool) -> void:
 	var zoom_step: float = 0.14
